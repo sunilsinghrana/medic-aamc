7078ef67d47c4ea929c33e4c75e285657c4232af
sunilsinghrana <suneilrana1944@gmail.com>
2023-07-22 20:34:09 +0530

    fix some api route

--- src/app/api/patients/route.ts
+++ src/app/api/patients/route.ts
@@ -1,20 +1,20 @@
 import connectMongoDB from "@/helper/db";
-import Patient from "@/models/Patient";
+import Patient from "@/models/PatientSchema";
 import { NextRequest, NextResponse } from "next/server";
 
 
-// Get all medicines
+// Get all patients
 export const GET =async (req:NextRequest, res: NextResponse) => {
     try {
-        await connectMongoDB();
+        await connectMongoDB(); 
         const patient = await Patient.find();
         return NextResponse.json(patient)
     } catch (error) {
-        return NextResponse.json(error)
+        return NextResponse.json({message: "failed to get patients", error})
     }
 }
 
-// Create new medicine
+// Create new patient
 export const POST =async (req:NextRequest, res: NextResponse) => {
     const { name, age, gender, phoneNumber } = await req.json();
     try {
