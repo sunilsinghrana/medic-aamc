c5e20ca143b3ceac1bc0ab3ea60bea954ae0c83f
sunilsinghrana <suneilrana1944@gmail.com>
2023-08-21 19:43:02 +0530

    fix bugs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ThemeProvider } from './provider'
-import Navigation from '@/components/Navigation'
 import Navbar from '@/components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
